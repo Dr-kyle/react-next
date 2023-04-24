be23ba0a926cf133f94489199bd1d16350e264c2
Dr-kyle <13636814471@163.com>
2023-04-24 18:50:04 +0800

    update

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -1,17 +1,27 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
+import { authOptions } from './auth/[...nextauth]'
+import { getServerSession } from "next-auth/next"
 // Create proxy instance outside of request handler function to avoid unnecessary re-creation
+import { createProxyMiddleware } from 'http-proxy-middleware';
 console.log('process.env.TEST', process.env.TEST)
 const apiProxy = createProxyMiddleware({
     target: 'http://localhost:5000',
-    changeOrigin: false,
+    changeOrigin: true,
     // pathRewrite: { [`^/api/proxy`]: '' },
     toProxy: true,
     secure: false,
-    logger: console,
+    logger: console
+    // on: {
+    //   proxyReq: (proxyReq, req, res) => {
+    //     // const session = await getServerSession(req, res, authOptions)
+    //     // console.log('session', session)
+    //     proxyReq.setHeader('x-added', 'foobar');
+    //   }
+    // }
 });
 
+
 export default function handler (req, res) {
+    req.headers.token = 'aaaaaaa'
     apiProxy(req, res, (result) => {
         if (result instanceof Error) {
             throw result;
